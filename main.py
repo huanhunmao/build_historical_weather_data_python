96499d3197d85116ed3875853077df5aebaa86a8
Marxu <fuhaojun@52tt.com>
2024-06-11 22:51:59 +0800

    feat: 加一个 api/word  用来解决 键入一个单词 得到它的 全大写

--- main.py
+++ main.py
@@ -6,16 +6,15 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route('/api/v1/<station>/<date>')
-def about(station, date):
-    temperature = 23
+@app.route('/api/v1/<word>')
+def about(word):
+    definition = word.upper()
     return {
-        "station": station,
-        "date": date,
-        "temperature": temperature
+        "definition": definition,
+        "word": word
     }
 
 # 确保当这个脚本文件被直接运行时，才会执行 app.run(debug=True) 这行代码
 # 而在被导入时，这段启动代码不会执行 可导入这个脚本的其他方法使用
 if __name__ == '__main__':
-    app.run(debug=True, port=5002)
+    app.run(debug=True)
