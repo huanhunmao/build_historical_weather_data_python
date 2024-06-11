b23f297336dead4e66ee5e8ec498007ef843468e
Marxu <fuhaojun@52tt.com>
2024-06-11 22:02:23 +0800

    feat: 解决多个 app 端口占用问题

--- main.py
+++ main.py
@@ -18,4 +18,4 @@ def about(station, date):
 # 确保当这个脚本文件被直接运行时，才会执行 app.run(debug=True) 这行代码
 # 而在被导入时，这段启动代码不会执行 可导入这个脚本的其他方法使用
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5002)
