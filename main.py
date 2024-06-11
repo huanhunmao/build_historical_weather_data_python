6f47e6628370c79cb326b86bc8f1c2264cf806ae
Marxu <fuhaojun@52tt.com>
2024-06-11 22:00:36 +0800

    feat: 添加最简 api 示例和返回

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from flask import Flask, render_template
 
-app = Flask('Website')
+app = Flask(__name__)
 
-@app.route('/home')
+@app.route('/')
 def home():
-    return render_template('tutorial.html')
+    return render_template('home.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/api/v1/<station>/<date>')
+def about(station, date):
+    temperature = 23
+    return {
+        "station": station,
+        "date": date,
+        "temperature": temperature
+    }
 
-app.run(debug=True)
+# 确保当这个脚本文件被直接运行时，才会执行 app.run(debug=True) 这行代码
+# 而在被导入时，这段启动代码不会执行 可导入这个脚本的其他方法使用
+if __name__ == '__main__':
+    app.run(debug=True)
